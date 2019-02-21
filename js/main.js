d29e01af9f8a98dc457cc87b2537015f6a37f4a1
Kleff <kleff@gmail.com>
2019-02-21 08:28:08 -0300

    version 2.0 improved

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 
+
+let url = "https://api.punkapi.com/v2/beers?page=1&per_page=10";
+document.getElementById('moreBeers').addEventListener('click', moreBeers);
+
+function showErrorMessage(error) {
+    let message = `
+        <p>Sorry, there was a problem. We are working to resolve the issue as quickly as possible. Error: ${error}</p>
+        <a href="index.html">Back to Home</a>
+        `;
+    document.getElementById('error-message').innerHTML = message;
+};
+
 function getBeers() {
-    fetch('https://api.punkapi.com/v2/beers?page=1&per_page=10')
+    fetch(url)
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
@@ -28,63 +40,19 @@ function getBeers() {
             document.getElementById('beersList').innerHTML = output;
         })
         .catch(function(error) {
-            // Clean beerList div
+            // Cleaning beerList
             document.getElementById('beersList').innerHTML = " ";
-            let message = `
-                <p>Sorry, there was a problem. We are working to resolve the issue as quickly as possible. Error: ${error}</p>
-                <a href="/">Back to Home</a>
-                `;
-            document.getElementById('error-message').innerHTML = message;
+            showErrorMessage(error);
             // Hide button moreBeers
             document.getElementById('moreBeers').style.display = "none";
           });
         
-}
+};
+
+function moreBeers() {
+    // Change url variable to any unknown url on purpose because I want it to throw an error.
+    url = "https://api.punkapi.com/v2/thisUrlDoesntExist/";
+    getBeers()
+};
 
 getBeers()
-
-
-document.getElementById('moreBeers').addEventListener('click', moreBeers);
-function moreBeers() {
-    fetch('https://api.punkapi.com./thiswebdoesntexist')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            let output = '<h2>Take a look at our Beers!</h2>'
-            data.forEach(function (beer) {
-                output += `
-                    <div class="beer-item">
-                        <img src="${beer.image_url}" alt="${beer.name}"/>
-                        <div class="beer-name">
-                            <h3>${beer.name}</h3>
-                            <p>${beer.tagline}</p>
-                        </div>
-                        <div class="ibu-ph">
-                            <h3>${beer.ibu}</h3>
-                            <p>IBU</p>
-                        </div>
-                        <div class="ibu-ph">
-                            <h3>${beer.ph}</h3>
-                            <p>pH</p>
-                        </div>
-                    </div>
-                    <hr>
-                `
-            });
-            document.getElementById('beersList').innerHTML = output;
-        })
-        .catch(function(error) {
-            // Clean beerList div
-            document.getElementById('beersList').innerHTML = " ";
-            let message = `
-                <p>Sorry, there was a problem. We are working to resolve the issue as quickly as possible. Error: ${error}</p>
-                <a href="/">Back to Home</a>
-                `;
-            document.getElementById('error-message').innerHTML = message;
-            // Hide button moreBeers
-            document.getElementById('moreBeers').style.display = "none";
-          });
-}
-
-/* Aclaración: Puede mejorarse el codigo mediante ciertos cambios. Por ejemplo, puede reutilizarse la misma función solo cambiando la dirección de FETCH utilizando alguna variable que contenga un string con la direccion correcta y que al apretar el boton la direccion cambie (sobreescribiendo la variable) por una erronea para que el catch capture el error y muestre el error-message.
-*/
